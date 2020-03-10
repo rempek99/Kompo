594e381cb14f21d0fa4c905b82f569d2ed6bee2c
rempek99 <arkadiusz.remplewicz@gmail.com>
2020-03-10 13:32:33 +0100

    Zadanie 2

--- Remplewicz/src/MojaKlasa.java
+++ Remplewicz/src/MojaKlasa.java
@@ -1,6 +1,29 @@
 import javax.swing.*;
+import java.util.Arrays;
+import java.util.Date; 
 
 public class MojaKlasa {
+	
+	static int [] babelkowe(int tab[], int rozmiar)
+	{
+		int posortowana[] = new int[rozmiar];
+		for (int i = 0; i < rozmiar; i++)
+			posortowana[i] = tab[i];
+		int tmp = 0;
+		for (int i = 0; i < rozmiar; i++)
+		{
+			for (int j = 1; j < rozmiar - i; j++)
+			{
+				if(posortowana[j-1]>posortowana[j])
+				{
+					tmp = posortowana[j];
+					posortowana[j] = posortowana[j-1];
+					posortowana[j-1] = tmp;
+				}
+			}
+		}
+		return posortowana;
+	}
 
 	static long strong(int n)
 	{
@@ -25,6 +48,7 @@ public class MojaKlasa {
 	}
 	
 	public static void main(String[] args) {
+////////// TYDZIEN 1. ////////////////////////////////////////////////	
 //		System.out.println("Hello world!!!");
 //		System.out.println("Inny napis");
 //		
@@ -50,26 +74,134 @@ public class MojaKlasa {
 //		int liczba2 = Integer.parseInt(txt2);
 //		System.out.println("Po konwersji1");
 //		System.out.println(liczba1 + liczba2);
-		String wiersze;
-		wiersze = JOptionPane.showInputDialog("Podaj liczbe wierszy");
-		String output = "";
-		int n = Integer.parseInt(wiersze);
-		for(int i=0;i<n;i++)
+//		String wiersze;
+//		wiersze = JOptionPane.showInputDialog("Podaj liczbe wierszy");
+//		String output = "";
+//		int n = Integer.parseInt(wiersze);
+//		for(int i=0;i<n;i++)
+//		{
+//			for(int j=0;j<n-i;j++)
+//			{
+//				output += "    ";
+//			}
+//			for(int j=0;j<i+1;j++)
+//			{
+//				output += pascal(i,j);
+//				output += "       ";
+//			}
+//			output += "\n";
+//		}
+//		System.out.print(output);
+//
+/////// 		TYDZIEN 02 		////////////////////////////////////////////
+//		System.out.println(args[0]);
+//		System.out.println(args[1]+" | "+args[2]);
+		
+//		if (args.length < 3)
+//			System.err.print("Za malo argumentow");
+//		else
+//			System.out.println(" * " + args[0]+" ** " + args[1] + " *** " + args[2]);
+//		
+		
+///////////  	ARGUMENTY WYWOLANIA PROGRAMU	////////////////////////	
+//		int i;
+//		System.out.println("Wczytano agrumenty: ");
+//		for (i = 0; i<args.length;i++)
+//		{
+//			System.out.print(args[i]+" ");
+//		}
+//		System.out.print("\n");
+//		boolean good=true;
+//		double num1 = Double.parseDouble(args[0]);
+//		double num2 = Double.parseDouble(args[1]);
+//		double wynik = 0;
+//		switch(args[2])
+//		{
+//		case "+":
+//		{
+//			wynik = num1+num2;
+//			break;
+//		}
+//		case "-":
+//		{
+//			wynik = num1-num2;
+//			break;
+//		}
+//		case "*":
+//		{
+//			wynik = num1*num2;
+//			break;
+//		}
+//		case "/":
+//		{
+//			if(num2==0)
+//			{
+//				System.err.print("Proba dzielenia przez 0");
+//				good = false;
+//			}
+//			wynik = num1/num2;
+//			break;
+//		}
+//		}
+//		if(good)
+//		System.out.println("Wynik: "+ wynik);
+		
+///////////// TABLICE: INDEKSOWANIE, WYPELNIANIE, PRZESZUKIWANIE /////////////////
+//		int i;
+//		
+//		int tab[] = new int[10];
+//		for (i = 1; i<10; i=i+1)
+//		{
+//			tab[i]=5*i+3;
+//		}
+//		for (i = 1; i < 10; i=i+1)
+//			System.out.print(tab[i]+" ");
+//		
+//		System.out.println("");
+//		
+///////////////////////////////////////////////////////////////////////////////
+		int i = 0;
+		String txt1;
+		txt1 = JOptionPane.showInputDialog("Ilosc liczb do wylowoania");
+		i = Integer.parseInt(txt1);
+		int rozmiar = i;
+		int tablica_inna[] = new int[rozmiar];
+		
+		for (i = 0; i < rozmiar; i++)
 		{
-			for(int j=0;j<n-i;j++)
-			{
-				output += "    ";
-			}
-			for(int j=0;j<i+1;j++)
-			{
-				output += pascal(i,j);
-				output += "       ";
-			}
-			output += "\n";
+			tablica_inna[i] = 20-2*i;
+			//System.out.print(tablica_inna[i]+" ");
 		}
-		System.out.print(output);
+		//System.out.println("");
 		
+		int zakres = 100;
+		for (i = 0; i < rozmiar; i++)
+			tablica_inna[i] = (int)(Math.random()*zakres);
+		System.out.println("Wylosowana tablica");
+		for (i = 0; i< rozmiar; i++)
+			System.out.print(tablica_inna[i]+" ");
+		System.out.println("");
 		
+		Date przed_babelkowym = new Date();
+		int posortowana [] = babelkowe(tablica_inna, rozmiar);
+		Date po_babelkowym = new Date();
+		
+		System.out.println("Po sortowaniu babelkowym");
+		for (i = 0; i< rozmiar; i++)
+			System.out.print(posortowana[i]+" ");
+		System.out.println("");
+		
+		Date przed_arrays = new Date();
+		Arrays.sort(tablica_inna);
+		Date po_arrays = new Date();
+		
+		System.out.println("Po sortowaniu Arrays");
+		for (i = 0; i< rozmiar; i++)
+			System.out.print(tablica_inna[i]+" ");
+		System.out.println("");
+		
+		System.out.println("Sortowanie babelkowe zajelo: "+ (po_babelkowym.getTime()-przed_babelkowym.getTime())+"ms");
+		System.out.println("Sortowanie arrays zajelo: "+ (po_arrays.getTime()-przed_arrays.getTime())+"ms");
 	}
 
 }
